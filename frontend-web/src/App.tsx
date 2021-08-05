36e76de1185a4810830f37934c3c1eb9997be83f
Glauco Todesco <glauco.todesco@gmail.com>
2021-08-05 13:04:58 -0300

    Add BootStrap and Header

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,9 +1,13 @@
-import './App.css';
+import './core/assets/styles/custom.scss';
+import './App.scss';
+import Header from 'core/components/Header';
 
 function App() {
   return (
     <div>
+        <Header/>
         <h1>Create Project</h1>
+
     </div>
   );
 }
---
+++ frontend-web/src/core/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import './styles.scss';
+
+const Header = () => (
+   <div className="header-main">
+       <h1 className="header-title">
+            MovieFlix
+       </h1>
+   </div>
+)
+
+export default Header;
