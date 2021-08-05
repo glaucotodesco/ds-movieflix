52c47db71ade58f886aff0d8b168cea06522b793
Glauco Todesco <glauco.todesco@gmail.com>
2021-08-05 20:07:35 -0300

    Finished Home Layout

--- frontend-web/src/pages/Home/index.tsx
+++ frontend-web/src/pages/Home/index.tsx
@@ -1,7 +1,36 @@
 import './styles.scss';
+import { ReactComponent as Banner } from 'core/assets/images/banner.svg';
+
 
 const Home = () => (
-    <h1>Home</h1>
+    <div className="home-container">
+        <div className="row col-3">
+            <div className="home-title">
+                Avalie Filmes
+            </div>
+            <div className="home-text">
+                Diga o que você achou do seu <br />
+                filme favorito
+            </div>
+            <div>
+                <Banner className="home-banner" />
+            </div>
+        </div>
+        <div className="row">
+            <div className="card-base home-login">
+                <div className="home-login-text">
+                    LOGIN
+                </div>
+                <input className="form-control input-base" placeholder="Email" />
+                <input className="form-control input-base" placeholder="Senha" />
+                <button className="btn btn-primary button-base btn-login">
+                    FAZER LOGIN
+                </button>
+            </div>
+
+        </div>
+    </div>
+
 )
 
 export default Home;
