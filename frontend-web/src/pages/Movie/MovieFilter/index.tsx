de46a3736a41dd554f1f1b9e3c70dd8d96f4a9f5
Glauco Todesco <glauco.todesco@gmail.com>
2021-08-17 16:11:49 -0300

    OnChange Genres finished

--- frontend-web/src/pages/Movie/MovieFilter/index.tsx
+++ frontend-web/src/pages/Movie/MovieFilter/index.tsx
@@ -1,9 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import { Controller, SubmitHandler, useForm } from "react-hook-form";
-
 import Select from "react-select";
-
 import { makePrivateRequest } from "core/utils/request";
 import { Genre } from "core/types/Genre";
 import './styles.scss';
@@ -12,27 +9,15 @@ type FormState = {
     genres: Genre,
 };
 
+type Props = {
+    onChangeGenres: any;
+    genres: Genre[];
+}
 
-const MovieFilter = () => {
+const MovieFilter = ({ genres, onChangeGenres }: Props) => {
 
-    const [genres, setGenres] = useState<Genre[]>();
-    const [genre, setGenre] = useState<Genre>();
-
-    useEffect(() => {
-        makePrivateRequest({ url: '/genres' })
-            .then(
-                response => {
-                    const merged = [
-                        ...[{"id": 0, "name": "TODOS"}],
-                        ...response.data
-                    ]
-                    setGenres(merged);
-                }
-            )
-    }, []);
-
-    const onSubmit = (val: any) => {
-        console.log(val);
+    const onChange = (val: any) => {
+        onChangeGenres(val);
     }
 
     return (
@@ -45,7 +30,7 @@ const MovieFilter = () => {
                         getOptionValue={(option: Genre) => String(option.id)}
                         classNamePrefix="genre-select"
                         placeholder="Gêneros"
-                        onChange={(val) => onSubmit(val)}
+                        onChange={(val) => onChange(val)}
                     />
                 </form>
             </div>
--- frontend-web/src/pages/Movie/index.tsx
+++ frontend-web/src/pages/Movie/index.tsx
@@ -4,21 +4,24 @@ import Pagination from 'core/components/Pagination';
 import { useEffect, useState } from 'react';
 import MovieCard from './MovieCard';
 import './styles.scss';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import MovieFilter from './MovieFilter';
+import { Genre } from 'core/types/Genre';
 
 const Movie = () => {
 
     const [activePage, setActivePage] = useState(0);
+    const [genreId, setGenreId] = useState(0);
+    const [genres, setGenres] = useState<Genre []>([]);
     const [moviesResponse, setMoviesResponse] = useState<MoviesResponse>();
     const [numberOfElements, setNumberOfElements] = useState<number>();
     const linesPerPage: number = 4;
 
     useEffect(() => {
-
         const params = {
             page: activePage,
-            linesPerPage: linesPerPage
+            linesPerPage: linesPerPage,
+            genreId : genreId
         };
 
         makePrivateRequest({ url: '/movies', params })
@@ -27,12 +30,30 @@ const Movie = () => {
                 setNumberOfElements(response.data.numberOfElements);
                 }
             )
-    }, [activePage]);
+    }, [activePage,genreId]);
+
+    useEffect(() => {
+        makePrivateRequest({ url: '/genres' })
+            .then(
+                response => {
+                    const merged = [
+                        ...[{"id": 0, "name": "TODOS"}],
+                        ...response.data
+                    ]
+                    setGenres(merged);
+                }
+            )
+    }, []);
+
+
+    const onChangeGenre = (genre: Genre) => {
+        setGenreId(genre.id);
+    }
 
     return (
         <>
             <div className="movie-filter-container">
-                <MovieFilter />
+                <MovieFilter onChangeGenres={onChangeGenre} genres={genres} />
             </div>
 
             <div className="movie-container">
@@ -52,8 +73,7 @@ const Movie = () => {
                         totalPages={moviesResponse.totalPages}
                         activePage={activePage}
                         onChange={page => setActivePage(page)}
-
-                    />
+                   />
                 }
             </div>
         </>
