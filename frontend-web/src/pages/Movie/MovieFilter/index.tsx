060f60b5b4e39620753114c9c9ce503f1358524e
Glauco Todesco <glauco.todesco@gmail.com>
2021-08-17 15:07:50 -0300

    Added onChange event on Filter Genres

--- frontend-web/src/pages/Movie/MovieFilter/index.tsx
+++ frontend-web/src/pages/Movie/MovieFilter/index.tsx
@@ -1,33 +1,53 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+
 import Select from "react-select";
 
 import { makePrivateRequest } from "core/utils/request";
 import { Genre } from "core/types/Genre";
 import './styles.scss';
 
+type FormState = {
+    genres: Genre,
+};
 
 
 const MovieFilter = () => {
 
-    const [genres, setGenres] = useState<Genre[]>([]);
+    const [genres, setGenres] = useState<Genre[]>();
+    const [genre, setGenre] = useState<Genre>();
 
     useEffect(() => {
         makePrivateRequest({ url: '/genres' })
             .then(
                 response => {
-                    setGenres(response.data);
+                    const merged = [
+                        ...[{"id": 0, "name": "TODOS"}],
+                        ...response.data
+                    ]
+                    setGenres(merged);
                 }
             )
     }, []);
 
+    const onSubmit = (val: any) => {
+        console.log(val);
+    }
+
     return (
         <div className="movie-filter card-base">
             <div className="select-item-size" >
-                <Select options={genres} 
-                        getOptionLabel={ (option: Genre) => option.name}
-                        getOptionValue={ (option: Genre) => String(option.id)}
-                        classNamePrefix="category-select" />
+                <form>
+                    <Select
+                        options={genres}
+                        getOptionLabel={(option: Genre) => option.name}
+                        getOptionValue={(option: Genre) => String(option.id)}
+                        classNamePrefix="genre-select"
+                        placeholder="Gêneros"
+                        onChange={(val) => onSubmit(val)}
+                    />
+                </form>
             </div>
         </div>
     )
